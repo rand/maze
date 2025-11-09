f487aa6fee09c0d99a36242bd5be8aa9b39d438c
Rand Arete <rand.arete@gmail.com>
2025-11-09 12:27:47 -0700

    Update learning module exports for all Phase 5 components

--- src/maze/learning/__init__.py
+++ src/maze/learning/__init__.py
@@ -11,11 +11,62 @@ from maze.learning.pattern_mining import (
     SemanticPattern,
     PatternSet
 )
+from maze.learning.constraint_learning import (
+    ConstraintLearningSystem,
+    ConstraintRefinement,
+    Feedback,
+    GenerationResult,
+    ValidationResult,
+    RepairResult,
+    LearningEvent,
+)
+from maze.learning.project_adaptation import (
+    ProjectAdaptationManager,
+    ProjectProfile,
+    ConventionSet,
+    AdaptationStats,
+)
+from maze.learning.feedback_orchestrator import (
+    FeedbackLoopOrchestrator,
+    FeedbackResult,
+    FeedbackStats,
+)
+from maze.learning.hybrid_weighting import (
+    HybridConstraintWeighter,
+    WeightedConstraintSet,
+    TokenWeights,
+    ConstraintSet,
+    GenerationState,
+)
 
 __all__ = [
+    # Pattern mining
     "PatternMiningEngine",
     "SyntacticPattern",
     "TypePattern",
     "SemanticPattern",
     "PatternSet",
+    # Constraint learning
+    "ConstraintLearningSystem",
+    "ConstraintRefinement",
+    "Feedback",
+    "GenerationResult",
+    "ValidationResult",
+    "RepairResult",
+    "LearningEvent",
+    # Project adaptation
+    "ProjectAdaptationManager",
+    "ProjectProfile",
+    "ConventionSet",
+    "AdaptationStats",
+    # Feedback orchestration
+    "FeedbackLoopOrchestrator",
+    "FeedbackResult",
+    "FeedbackStats",
+    # Hybrid weighting
+    "HybridConstraintWeighter",
+    "WeightedConstraintSet",
+    "TokenWeights",
+    "ConstraintSet",
+    "GenerationState",
 ]
